08b273b3d3dc00c3f96f208a7226a94640a2243e
Guy Ezer <guyezer@localhost.localdomain>
2019-02-26 22:41:09 +0200

    fixed compilation bug

--- compile.sh
+++ compile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 export LANG=C
-export ARCH_DATA_MODEL=64
-export DISABLE_HOTSPOT_OS_VERSION_CHECK=ok
-export ALT_BOOTDIR=/usr/local/java/jdk1.6.0_45 jvmg
+#export ARCH_DATA_MODEL=64
+#export DISABLE_HOTSPOT_OS_VERSION_CHECK=ok
+export ALT_BOOTDIR=/specific/disk1/home/mad/Galois-PQ/inputs/students/guy/project/jdk1.6.0_45 jvmg
 export ALLOW_DOWNLOADS=true
-export USE_PRECOMPILED_HEADER=true
-export SKIP_DEBUG_BUILD=false
-export SKIP_FASTDEBUG_BUILD=true
-export DEBUG_NAME=debug
+#export USE_PRECOMPILED_HEADER=true
+#export SKIP_DEBUG_BUILD=false
+#export SKIP_FASTDEBUG_BUILD=true
+#export DEBUG_NAME=debug
 unset CLASSPATH
 unset JAVA_HOME
 python -m SimpleHTTPServer 22122 &
