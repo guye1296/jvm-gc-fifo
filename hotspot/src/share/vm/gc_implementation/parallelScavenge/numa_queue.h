4f04230dd8ac23deced2e9585b29ea212a2ec507
guye1296 <ezer.guy@gmail.com>
2019-03-16 13:23:05 +0200

    removed redundant headers

--- hotspot/src/share/vm/gc_implementation/parallelScavenge/numa_queue.h
+++ hotspot/src/share/vm/gc_implementation/parallelScavenge/numa_queue.h
@@ -3,10 +3,10 @@
 
 #include "numa_globals.h"
 
-extern void numa_enqueue(Globals* context, Object arg, int pid);
+extern "C" void numa_enqueue(Globals* context, Object arg, int pid);
 
-extern Object numa_dequeue(Globals* context, int pid);
+extern "C" Object numa_dequeue(Globals* context, int pid);
 
-extern Globals* create_global_context();
+extern "C" Globals* create_global_context();
 
 #endif //_NUMA_QUEUE_H
