b89808978007856fc866a6ddff2c7d97ba34af1b
ranhoch <ranhoch@localhost>
2019-03-17 01:26:19 +0200

    Removed debug prints

--- hotspot/src/share/vm/gc_implementation/parallelScavenge/gcTaskManager.cpp
+++ hotspot/src/share/vm/gc_implementation/parallelScavenge/gcTaskManager.cpp
@@ -137,7 +137,6 @@ GCTaskQueue* GCTaskQueue::create() {
     tty->print_cr("GCTaskQueue::create()"
                   " returns " INTPTR_FORMAT, result);
   }
-  printf("GCTaskQueue::create() ==> (%zu)\n", (size_t)result);
   return result;
 }
 
@@ -148,7 +147,6 @@ GCTaskQueue* GCTaskQueue::create_on_c_heap() {
                   " returns " INTPTR_FORMAT,
                   result);
   }
-  printf("GCTaskQueue::create_on_c_heap() ==> (%zu)\n", (size_t)result);
   return result;
 }
 
@@ -176,7 +174,6 @@ GCTaskQueue::~GCTaskQueue() {
 #endif
 
 void GCTaskQueue::destruct() {
-    printf("GCTaskQueue::destruct free memory\n");
     free(context);
 }
 
@@ -200,7 +197,6 @@ void GCTaskQueue::destroy(GCTaskQueue* that) {
 }
 
 void GCTaskQueue::initialize() {
-    printf("GCTaskQueue::initialize\n");
     if (!_is_c_heap_obj) {
         set_insert_end(NULL);
         set_remove_end(NULL);
@@ -208,7 +204,6 @@ void GCTaskQueue::initialize() {
     }
     else {
         if (context == NULL) {
-            printf("GCTaskQueue::initialize create new memory\n");
             context = create_global_context();
         }
         set_length(0);
@@ -345,7 +340,6 @@ GCTask* GCTaskQueue::dequeue() {
 
 // Dequeue one task, preferring one with affinity.
 GCTask* GCTaskQueue::dequeue(uint affinity) {
-  printf("guy: entring affinity dequeue\n");
   if (TraceGCTaskQueue) {
     tty->print_cr("[" INTPTR_FORMAT "]"
                   " GCTaskQueue::dequeue(%u)", this, affinity);
@@ -516,11 +510,8 @@ void GCTaskManager::initialize() {
   _queue = SynchronizedGCTaskQueue::create(unsynchronized_queue, lock());
   _noop_task = NoopGCTask::create_on_c_heap();
   reset_noop_tasks();
-  printf("unsynchronized_queue(%zu)\n", (size_t)unsynchronized_queue);
-  printf("_queue(%zu)\n", (size_t)_queue);
 #else
   _queue = GCTaskQueue::create_on_c_heap();
-  printf("_queue(%zu)\n", (size_t)_queue);
 #ifdef NUMA_AWARE_TASKQ
   if (UseGCTaskAffinity && UseNUMA) {
     uint n = os::numa_get_groups_num();
@@ -529,10 +520,8 @@ void GCTaskManager::initialize() {
       _numa_queues->append(GCTaskQueue::create_on_c_heap());
 
     for (uint i=0; i < n; i++)
-      printf("_numa_queues[%u](%zu)\n", i, (size_t)_numa_queues[i]);
   } else
     _numa_queues = NULL;
-    printf("_numa_queues is NULL\n");
 #endif
 #ifdef INTER_NODE_MSG_Q
   if (UseNUMA) {
@@ -699,7 +688,6 @@ void GCTaskManager::add_task(GCTask* task) {
                   task, GCTask::Kind::to_string(task->kind()));
   }
   queue()->enqueue(task);
-  //printf("GCTaskManager(%zu)::add_task queue=%zu, task=%zu\n", (size_t)this, (size_t)queue(), (size_t)task);
 #ifndef REPLACE_MUTEX
   // Notify with the lock held to avoid missed notifies.
   if (TraceGCTaskManager) {
@@ -750,7 +738,6 @@ void GCTaskManager::add_list(GCTaskQueue* list) {
       _numa_queues->at(i)->enqueue(numa_q + i);
     }
     queue()->enqueue(&q);
-    printf("GCTaskManager::add_list call initialize");
     list->initialize();
   } else
 #endif
@@ -807,7 +794,6 @@ tryagain:
   } else
 #endif
   result = queue()->dequeue();
-  //printf("GCTaskManager(%zu)::get_task queue=%zu, task=%zu\n", (size_t)this, (size_t)queue(), (size_t)result);
   if (result) {
     if (result->is_barrier_task()) {
       assert(which != sentinel_worker(),
